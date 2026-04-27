bc3dd0b59ef00b74f5050a042a6526ca350413de
Illumidragui <shengjun.yech@proton.me>
2026-04-27 18:57:38 +0200

    destroy also deletes device from tailscale dashboard. Cleaning outputs. arn is in env and not secret

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,3 @@
-output "instance_ip" {
-  description = "Private IP of the k3s EC2 instance (access via Tailscale)"
-  value       = module.ec2.instance_ip
-}
-
 output "tailscale_hostname" {
   description = "Hostname of the k3s node in the Tailscale network"
   value       = module.ec2.tailscale_hostname
